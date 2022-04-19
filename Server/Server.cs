91bf9d32957b4d7e7f025a20f7a458fb5c543e9f
Lakasabasz <lakasabasz@gmail.com>
2022-04-19 17:09:05 +0200

    Init

---
+++ Server/Server.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MRCR
+{
+    class ServerApp
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
