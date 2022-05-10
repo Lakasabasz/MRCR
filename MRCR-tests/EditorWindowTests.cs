c174cea724ad3420d57e2dde862bfaaf286a5234
Lakasabasz <lakasabasz@gmail.com>
2022-05-10 13:41:18 +0200

    Fix bug with disapearing tooltips and reorganize tests

--- MRCR-tests/EditorWindowTests.cs
+++ MRCR-tests/EditorWindowTests.cs
@@ -35,6 +35,14 @@ public class EditorWindowTests
         return filename;
     }
 
+    [Test, Apartment(ApartmentState.STA), NonParallelizable, Explicit]
+    public void EditorWindowShowTest()
+    {
+        string filename = SetupWorldFile();
+        Editor editor = new Editor(filename);
+        editor.ShowDialog();
+    }
+
     [Test, Apartment(ApartmentState.STA), NonParallelizable, Order(1)]
     public void EditorWindowBeginStateTest()
     {
@@ -80,58 +88,7 @@ public class EditorWindowTests
         Assert.IsEmpty(editor.TreeLCS.Items);
         
         Assert.IsEmpty(editor.SpSoProperties.Children);
-        editor.ShowDialog();
-    }
-
-    [Test, Apartment(ApartmentState.STA), NonParallelizable, Order(2)]
-    public void EditorWindowButtonTest()
-    {
-        string filename = SetupWorldFile();
-        Editor editor = new Editor(filename);
-        ToolSetOrganizacja? tso = editor.ContentToolBar.Content as ToolSetOrganizacja;
-        tso.BtAddPost.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
-        Assert.IsTrue(tso.BtAddPost.IsChecked);
-        tso.BtAddDepot.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
-        Assert.IsTrue(tso.BtAddDepot.IsChecked);
-        Assert.IsFalse(tso.BtAddPost.IsChecked);
-        tso.BtAddStationDepot.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
-        Assert.IsTrue(tso.BtAddStationDepot.IsChecked);
-        Assert.IsFalse(tso.BtAddDepot.IsChecked);
-        tso.BtMove.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
-        Assert.IsTrue(tso.BtMove.IsChecked);
-        Assert.IsFalse(tso.BtAddStationDepot.IsChecked);
-        tso.BtSelect.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
-        Assert.IsTrue(tso.BtSelect.IsChecked);
-        Assert.IsFalse(tso.BtMove.IsChecked);
-        Assert.AreEqual(Visibility.Visible, tso.SWpSelect.Visibility);
-        Assert.IsTrue(tso.SBtSelectObject.IsChecked);
-        tso.SBtSelectTrail.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
-        Assert.IsTrue(tso.SBtSelectTrail.IsChecked);
-        Assert.IsFalse(tso.SBtSelectObject.IsChecked);
-        Assert.IsTrue(tso.BtSelect.IsChecked);
-        tso.SBtSelectLine.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
-        Assert.IsTrue(tso.SBtSelectLine.IsChecked);
-        Assert.IsFalse(tso.SBtSelectTrail.IsChecked);
-        tso.SBtSelectLCS.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
-        Assert.IsTrue(tso.SBtSelectLCS.IsChecked);
-        Assert.IsFalse(tso.SBtSelectLine.IsChecked);
-        tso.BtDelete.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
-        Assert.AreEqual(tso.SWpSelect.Visibility, Visibility.Collapsed);
-        Assert.IsFalse(tso.BtSelect.IsChecked);
-        Assert.IsFalse(tso.SBtSelectLCS.IsChecked);
-        Assert.IsTrue(tso.BtDelete.IsChecked);
-        Assert.IsTrue(tso.SBtDeleteObject.IsChecked);
-        tso.SBtDeleteTrail.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
-        Assert.IsTrue(tso.SBtDeleteTrail.IsChecked);
-        Assert.IsFalse(tso.SBtDeleteObject.IsChecked);
-        Assert.IsTrue(tso.BtDelete.IsChecked);
-        tso.SBtDeleteLine.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
-        Assert.IsTrue(tso.SBtDeleteLine.IsChecked);
-        Assert.IsFalse(tso.SBtDeleteTrail.IsChecked);
-        tso.SBtDeleteLCS.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
-        Assert.IsTrue(tso.SBtDeleteLCS.IsChecked);
-        Assert.IsFalse(tso.SBtDeleteLine.IsChecked);
-        tso.BtDelete.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        editor.Show();
     }
 
     [Test, Apartment(ApartmentState.STA), NonParallelizable, Order(3)]
--- MRCR-tests/ToolSetOrganizacjaTest.cs
+++ MRCR-tests/ToolSetOrganizacjaTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using MRCR.Editor_UC;
 using NUnit.Framework;
 
@@ -8,14 +9,66 @@ namespace MRCR_tests;
 
 public class ToolSetOrganizacjaTest
 {
-    [Test, Apartment(ApartmentState.STA)]
+    private Window _window;
+    [SetUp]
+    public void Setup()
+    {
+        _window = new Window();
+        _window.Content = new ToolSetOrganizacja();
+    }
+    
+    [Test, Apartment(ApartmentState.STA), Explicit]
     public void ToolSetOrganizacjaShowTest()
     {
-        ToolSetOrganizacja tso = new ToolSetOrganizacja();
-        Window w = new Window
-        {
-            Content = tso
-        };
-        w.ShowDialog();
+        _window.ShowDialog();
+    }
+    
+    [Test, Apartment(ApartmentState.STA)]
+    public void ButtonSingularStabilityTest()
+    {
+        ToolSetOrganizacja? tso = _window.Content as ToolSetOrganizacja;
+        tso.BtAddPost.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        Assert.IsTrue(tso.BtAddPost.IsChecked);
+        tso.BtAddDepot.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        Assert.IsTrue(tso.BtAddDepot.IsChecked);
+        Assert.IsFalse(tso.BtAddPost.IsChecked);
+        tso.BtAddStationDepot.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        Assert.IsTrue(tso.BtAddStationDepot.IsChecked);
+        Assert.IsFalse(tso.BtAddDepot.IsChecked);
+        tso.BtMove.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        Assert.IsTrue(tso.BtMove.IsChecked);
+        Assert.IsFalse(tso.BtAddStationDepot.IsChecked);
+        tso.BtSelect.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        Assert.IsTrue(tso.BtSelect.IsChecked);
+        Assert.IsFalse(tso.BtMove.IsChecked);
+        Assert.AreEqual(Visibility.Visible, tso.SWpSelect.Visibility);
+        Assert.IsTrue(tso.SBtSelectObject.IsChecked);
+        tso.SBtSelectTrail.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        Assert.IsTrue(tso.SBtSelectTrail.IsChecked);
+        Assert.IsFalse(tso.SBtSelectObject.IsChecked);
+        Assert.IsTrue(tso.BtSelect.IsChecked);
+        tso.SBtSelectLine.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        Assert.IsTrue(tso.SBtSelectLine.IsChecked);
+        Assert.IsFalse(tso.SBtSelectTrail.IsChecked);
+        tso.SBtSelectLCS.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        Assert.IsTrue(tso.SBtSelectLCS.IsChecked);
+        Assert.IsFalse(tso.SBtSelectLine.IsChecked);
+        tso.BtDelete.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        Assert.AreEqual(tso.SWpSelect.Visibility, Visibility.Collapsed);
+        Assert.IsFalse(tso.BtSelect.IsChecked);
+        Assert.IsFalse(tso.SBtSelectLCS.IsChecked);
+        Assert.IsTrue(tso.BtDelete.IsChecked);
+        Assert.IsTrue(tso.SBtDeleteObject.IsChecked);
+        tso.SBtDeleteTrail.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        Assert.IsTrue(tso.SBtDeleteTrail.IsChecked);
+        Assert.IsFalse(tso.SBtDeleteObject.IsChecked);
+        Assert.IsTrue(tso.BtDelete.IsChecked);
+        tso.SBtDeleteLine.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        Assert.IsTrue(tso.SBtDeleteLine.IsChecked);
+        Assert.IsFalse(tso.SBtDeleteTrail.IsChecked);
+        tso.SBtDeleteLCS.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+        Assert.IsTrue(tso.SBtDeleteLCS.IsChecked);
+        Assert.IsFalse(tso.SBtDeleteLine.IsChecked);
+        tso.BtDelete.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
     }
 }
